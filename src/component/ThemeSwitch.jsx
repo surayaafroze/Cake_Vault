d1bcc69ec46bc2d01b46ea7f515a91313966f0d3
surayaafroze63 <surayaafroze63@gmail.com>
2026-04-24 09:25:08 +0600

    add theme change button

--- src/component/ThemeSwitch.jsx
+++ src/component/ThemeSwitch.jsx
@@ -1,15 +1,36 @@
 "use client";
+import {Sun, Moon} from "@gravity-ui/icons";
+import {Switch} from "@heroui/react";
 
 
 import { useTheme } from "next-themes";
 
 const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme();
+  
   return (
     <div>
-       <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-   Toggle {theme === "dark" ? "Light" : "Dark"} Mode
-    </button>
+     <Switch onChange={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      {({isSelected}) => (
+        <>
+          <Switch.Control
+            className={`h-[31px] w-[51px] bg-[#ff66a3] ${isSelected ? "bg-[#ff66a3] shadow-[0_0_12px_rgba(6,182,212,0.5)]" : ""}`}
+          >
+            <Switch.Thumb
+              className={`size-[27px] bg-white shadow-sm ${isSelected ? "ms-[22px] shadow-lg" : ""}`}
+            >
+              <Switch.Icon>
+                {isSelected ? (
+                  <Sun className="size-4 text-[#ff66a3]" />
+                ) : (
+                  <Moon className="size-4 text-[#ff66a3]" />
+                )}
+              </Switch.Icon>
+            </Switch.Thumb>
+          </Switch.Control>
+        </>
+      )}
+    </Switch>
     </div>
   );
 };
